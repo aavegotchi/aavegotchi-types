352e43caa407cc9e084452e6d8a1f8de576c61f2
candoizo <candoizo@protonmail.com>
2021-08-31 09:17:40 -0400

    refresh staging

--- src/contracts/_common.ts
+++ src/contracts/_common.ts
@@ -1,33 +1,22 @@
-import { Contract, getDefaultProvider, providers } from "ethers";
-export const aavegotchiDiamondAddress =
-  "0x86935F11C86623deC8a25696E1C19a8659CbF95d";
-import abi from "./aavegotchi-contracts/diamondABI/diamond.json";
-// import requiredir from "require-dir";
-// const r = requiredir("./facet_abis");
-// const facet_names = Object.keys(r).map((x) => x);
+import { getDefaultProvider } from "@ethersproject/providers";
 
-const default_provider = getDefaultProvider(
-  "https://rpc-mainnet.maticvigil.com"
-);
+export const diamondAddress = {
+  137: "0x86935F11C86623deC8a25696E1C19a8659CbF95d",
+};
+
+export const addresses = {
+  diamond: {
+    137: "0x86935F11C86623deC8a25696E1C19a8659CbF95d",
+  },
+};
+
+const defaultProvider = (url?: string) => {
+  if (!url) url = "https://rpc-mainnet.maticvigil.com";
+  return getDefaultProvider(url);
+};
 
 export default {
-  aavegotchi_diamond(provider?: providers.Provider): Contract {
-    return new Contract(
-      aavegotchiDiamondAddress,
-      abi,
-      provider || default_provider
-    );
-  },
-
-  // facets(provider?: providers.Provider): Object {
-  //   let facets: Object;
-  //   facet_names.map((name) => {
-  //     return (facets[name] = new Contract(
-  //       aavegotchiDiamondAddress,
-  //       r[name],
-  //       provider || default_provider
-  //     ));
-  //   });
-  //   return facets;
-  // },
+  defaultProvider,
+  diamondAddress,
+  addresses,
 };
