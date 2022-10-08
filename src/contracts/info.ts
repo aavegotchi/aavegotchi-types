baf97c7988b381f1e75fd727acf5c73429da5997
candoizo <candoizo@protonmail.com>
2022-10-08 11:00:26 -0400

    build fixes for new contracts, cleanup

--- src/contracts/info.ts
+++
@@ -1,9 +0,0 @@
-import aavegotchi_diamond from "./_common";
-
-export default {
-
-  async totalAavegotchis(): Promise<number> {
-    return await aavegotchi_diamond.aavegotchi_diamond().totalSupply();
-  },
-
-}
